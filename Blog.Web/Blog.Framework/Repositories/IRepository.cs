e34ae29d5b31379a79419ec882ac16806d67d070
maaaaruf <marufbdonline@gmail.com>
2020-10-12 22:03:44 +0600

    Defiened CRUD  Methods in IRepository

---
+++ Blog.Web/Blog.Framework/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using Blog.Framework.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Framework.Repositories
+{
+    public interface IRepository<T> where T : IEntity
+    {
+        IQueryable<T> GetAll();
+        T GetById(int id);
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(int id);
+    }
+}
