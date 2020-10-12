cd71ab3ac9f18c8869ead344d6cff08a3d26ac08
maaaaruf <marufbdonline@gmail.com>
2020-10-12 21:49:04 +0600

    Added ArticleOverride

---
+++ Blog.Web/Blog.Data/Configurations/AutoMappingConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Blog.Framework.Entities;
+using FluentNHibernate.Automapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data.Configurations
+{
+    public class AutomappingConfiguration : DefaultAutomappingConfiguration
+    {
+        public override bool ShouldMap(Type type)
+        {
+            return type.GetInterface(typeof(IEntity).FullName) != null;
+        }
+    }
+}
