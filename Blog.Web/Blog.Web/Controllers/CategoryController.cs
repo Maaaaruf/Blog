41ec8032dd50e6a743704e158087fa5ad36b364f
maaaaruf <marufbdonline@gmail.com>
2020-10-13 09:34:05 +0600

    Added Category Controller

---
+++ Blog.Web/Blog.Web/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Blog.Framework.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Web.Controllers
+{
+    public class CategoryController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Category category)
+        {
+            return View();
+        }
+
+    }
+}
