df80f0fbdc7efbf0e26b1c4e5c2d9aaa1b83f737
maaaaruf <marufbdonline@gmail.com>
2020-10-16 08:02:53 +0600

    Added Framework Module

---
+++ Blog/Blog.Framework/Modules/FrameworkModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using Blog.Framework.Sessions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Blog.Framework.Modules
+{
+    public class FrameworkModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<FrameworkSession>()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
