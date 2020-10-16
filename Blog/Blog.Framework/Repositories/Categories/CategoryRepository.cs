9d47917558ce7b26fb3fa44d84580d78355dcc5e
maaaaruf <marufbdonline@gmail.com>
2020-10-16 23:47:23 +0600

    Added category repository

---
+++ Blog/Blog.Framework/Repositories/Categories/CategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using Blog.Data.BaseRepository;
+using Blog.Data.nHibernetConfigurations;
+using Blog.Framework.Entities;
+using Blog.Framework.Repositories.Articles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Framework.Repositories.Categories
+{
+    public class CategoryRepository : Repository<Category, InHibernetFrameworkSession>, ICategoryRepository
+    {
+        public CategoryRepository(InHibernetFrameworkSession sessionFactory) : base(sessionFactory)
+        {
+        }
+    }
+}
---
+++ Blog/Blog.Framework/Repositories/Categories/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using Blog.Data.BaseRepository;
+using Blog.Data.nHibernetConfigurations;
+using Blog.Framework.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Framework.Repositories.Categories
+{
+    public interface ICategoryRepository : IRepository<Category, InHibernetFrameworkSession>
+    {
+
+    }
+}
