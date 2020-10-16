76fe4326b41b16df94020d4c4adf9427da430b71
maaaaruf <marufbdonline@gmail.com>
2020-10-16 23:54:44 +0600

    Added Category UnitOfWork

---
+++ Blog/Blog.Framework/UnitOfWorks/Category/CategoryUnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using Blog.Data.BaseUnitOfWork;
+using Blog.Data.nHibernetConfigurations;
+using Blog.Framework.Repositories.Categories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Framework.UnitOfWorks.Category
+{
+    public class CategoryUnitOfWork : UnitOfWork, ICategoryUnitOfWork
+    {
+        public ICategoryRepository CategoryRepository { get; set; }
+        public CategoryUnitOfWork(InHibernetFrameworkSession session,
+            ICategoryRepository categoryRepository) 
+            : base(session)
+        {
+            CategoryRepository = categoryRepository;
+        }
+
+        
+    }
+}
---
+++ Blog/Blog.Framework/UnitOfWorks/Category/ICategoryUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Data.BaseUnitOfWork;
+using Blog.Framework.Repositories.Categories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Framework.UnitOfWorks.Category
+{
+    public interface ICategoryUnitOfWork : IUnitOfWork, IDisposable
+    {
+        public ICategoryRepository CategoryRepository { get; set; }
+    }
+}
