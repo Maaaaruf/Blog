862d479edd7c606f8ce5218a41eb265cb580f136
maaaaruf <marufbdonline@gmail.com>
2020-10-16 08:02:39 +0600

    Added FrameworkSession

--- Blog/Blog.Framework/Sessions/FrameworkSession.cs
+++ Blog/Blog.Framework/Sessions/FrameworkSession.cs
@@ -1,4 +1,5 @@
 ﻿using Blog.Framework.Entities;
+using Blog.Framework.Entities.Overrides;
 using Blog.Framework.Sessions.Configurations;
 using FluentNHibernate.Automapping;
 using FluentNHibernate.Cfg;
@@ -15,13 +16,18 @@ namespace Blog.Framework.Sessions
 {
     public class FrameworkSession
     {
+        public FrameworkSession()
+        {
+            Session = _sessionFactory.OpenSession();
+        }
+
         private static readonly ISessionFactory _sessionFactory;
         public ISession Session { get; set; }
 
         static FrameworkSession()
         {
             _sessionFactory = Fluently.Configure()
-                .Database(MsSqlConfiguration.MsSql2008.ConnectionString(x => x.FromConnectionStringWithKey("DefaultConnection")))
+                .Database(MsSqlConfiguration.MsSql2012.ConnectionString(x => x.FromConnectionStringWithKey("DefaultConnection")))
                 .Mappings(x => x.AutoMappings.Add(
                     AutoMap.AssemblyOf<Article>(new AutomappingConfiguration()).UseOverridesFromAssemblyOf<ArticleOverride>()))
                 .ExposeConfiguration(config => new SchemaUpdate(config).Execute(false, true))
