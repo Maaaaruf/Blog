5f36890fca15abf3bb7405ba799149187d60416e
maaaaruf <marufbdonline@gmail.com>
2020-10-16 08:01:12 +0600

    Added Entity Article

---
+++ Blog/Blog.Framework/Entities/Article.cs
@@ -0,0 +1,19 @@
+﻿using Blog.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Framework.Entities
+{
+    public class Article : IEntity
+    {
+        public virtual int Id { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string Descreption { get; set; }
+        public virtual DateTime PostedOn { get; set; }
+        public virtual DateTime EditedOn { get; set; }
+        public virtual bool isPublished { get; set; }
+    }
+}
