9679a895bccaabbd1acdf6e9b5d1362599f2addf
maaaaruf <marufbdonline@gmail.com>
2020-10-16 08:02:23 +0600

    Added Automapping configurations

---
+++ Blog/Blog.Framework/Sessions/Configurations/AutomappingConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Blog.Data;
+using FluentNHibernate.Automapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Framework.Sessions.Configurations
+{
+    public class AutomappingConfiguration : DefaultAutomappingConfiguration
+    {
+        public override bool ShouldMap(Type type)
+        {
+            return type.GetInterface(typeof(IEntity).FullName) != null;
+        }
+    }
+}
