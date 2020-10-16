641d2236960aa0541c570c412ab599e8a52c404e
maaaaruf <marufbdonline@gmail.com>
2020-10-16 23:54:22 +0600

    Changed Directory

---
+++ Blog/Blog.Framework/UnitOfWorks/Articles/IArticleUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Data.BaseUnitOfWork;
+using Blog.Framework.Repositories.Articles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Framework.UnitOfWorks.Articles
+{
+    public interface IArticleUnitOfWork : IUnitOfWork, IDisposable
+    {
+        public IArticleRepository ArticleRepository { get; set; }
+    }
+}
