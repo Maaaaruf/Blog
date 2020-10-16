37074ca0f8cfc9a329996bfd43145acd76d99705
maaaaruf <marufbdonline@gmail.com>
2020-10-16 08:01:28 +0600

    Added override for article entity

---
+++ Blog/Blog.Framework/Entities/Overrides/ArticleOverride.cs
@@ -0,0 +1,17 @@
+﻿using FluentNHibernate.Automapping;
+using FluentNHibernate.Automapping.Alterations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Framework.Entities.Overrides
+{
+    public class ArticleOverride : IAutoMappingOverride<Article>
+    {
+        public void Override(AutoMapping<Article> mapping)
+        {
+        }
+    }
+}
