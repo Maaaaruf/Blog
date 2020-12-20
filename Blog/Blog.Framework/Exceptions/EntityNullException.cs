623fd377473afa91f8426030e029ea9e19b0c1bb
maaaruf <marufbdonline@gmail.com>
2020-12-20 14:38:54 +0600

    Added Entity Null Exception

---
+++ Blog/Blog.Framework/Exceptions/EntityNullException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Framework.Exceptions
+{
+    public  class EntityNullException<T>:Exception
+    {
+        public Type EntityType { get; set; }
+        public EntityNullException()
+            :base($"Entity Null Exception for entity {typeof(T).FullName}")
+        {
+            EntityType = typeof(T);
+        }
+    }
+}
